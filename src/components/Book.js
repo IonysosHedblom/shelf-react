e4561e220d6bf0e126cce60b524582d2014fe936
Ionhedblom <ionysoshedblom@gmail.com>
2020-03-30 20:59:15 +0200

    Styles added

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,8 +7,6 @@ class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{
-              width: 128,
-              height: 193,
               backgroundImage: `url(${this.props.imageLinks.thumbnail})`
             }}>
             </div>
