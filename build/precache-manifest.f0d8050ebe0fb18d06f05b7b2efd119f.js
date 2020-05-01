ebd42e21e117e47eb88bde8aec18cfd4d6a18dfe
Ionhedblom <ionysoshedblom@gmail.com>
2020-05-01 19:13:25 +0200

    Final

---
+++ build/precache-manifest.f0d8050ebe0fb18d06f05b7b2efd119f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "68b6aaea688bc45b93d3",
+    "url": "/static/css/main.f020eeee.chunk.css"
+  },
+  {
+    "revision": "68b6aaea688bc45b93d3",
+    "url": "/static/js/main.68b6aaea.chunk.js"
+  },
+  {
+    "revision": "1aad1fcf49c65d13c175",
+    "url": "/static/js/1.1aad1fcf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "a22f017c3aab28967d198015a0e92e78",
+    "url": "/index.html"
+  }
+];
